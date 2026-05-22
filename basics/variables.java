e0174d9a607f52ab5a80b539529a11568b59387c
Kanan Bhardwaj <bhardwajkanan1013@gmail.com>
2026-05-22 06:36:05 -0700

    Create variables.java

---
+++ basics/variables.java
@@ -0,0 +1,11 @@
+class Variable {
+    public static void main(String[] args){
+        int age = 25;
+        float marks = 85.5f;
+        char grade = 'A';
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+    }    
+}
