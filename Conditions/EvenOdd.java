e82789d3a763a1bd7bfe48316ed68c964d8b3ffe
Kanan Bhardwaj <bhardwajkanan1013@gmail.com>
2026-05-22 09:24:07 -0700

    Added EvenOdd.java

---
+++ Conditions/EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class EvenOdd{
+    public static void main(String[] args){
+         int num;
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number:");
+        num = sc.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("The number is even.");
+        }
+        else{
+            System.out.println("The number is odd.");
+        }
+    }
+}
