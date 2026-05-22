57fb77ec93fa88ece7d1ade136538451b105f92e
Kanan Bhardwaj <bhardwajkanan1013@gmail.com>
2026-05-22 09:08:13 -0700

    Added DataTypes.java

---
+++ basics/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float percentage = 85.5f;
+        double salary = 25000.75;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Kanan";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
